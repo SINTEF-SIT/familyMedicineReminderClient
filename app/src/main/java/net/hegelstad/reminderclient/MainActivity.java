76474fe7f2b24fbe51fdee89f44f27c5f26450a5
Nikolai Hegelstad <nikolai@hegelstad.net>
2016-02-23 14:53:52 +0100

    cleaned up unused functions

--- app/src/main/java/net/hegelstad/reminderclient/MainActivity.java
+++ app/src/main/java/net/hegelstad/reminderclient/MainActivity.java
@@ -1,5 +1,6 @@
 package net.hegelstad.reminderclient;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,17 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
