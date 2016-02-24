98597c7019995e15946caa3959b0bafe82b827f6
Nikolai Hegelstad <nikolai@hegelstad.net>
2016-02-24 12:25:17 +0100

    added getters and setters

--- app/src/main/java/com/example/sondrehj/familymedicinereminderclient/Models/MedicationStorage.java
+++ app/src/main/java/com/example/sondrehj/familymedicinereminderclient/Models/MedicationStorage.java
@@ -7,4 +7,28 @@ public class MedicationStorage {
     String ownerId;
     String name;
     Double count;
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCount() {
+        return count;
+    }
+
+    public void setCount(Double count) {
+        this.count = count;
+    }
 }
