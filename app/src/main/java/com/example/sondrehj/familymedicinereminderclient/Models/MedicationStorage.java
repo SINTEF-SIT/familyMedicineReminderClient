ba4a2cf9caed9f2fecd69fe5cbdf0ee961158059
Nikolai Hegelstad <nikolai@hegelstad.net>
2016-02-24 12:18:20 +0100

    added three models

---
+++ app/src/main/java/com/example/sondrehj/familymedicinereminderclient/Models/MedicationStorage.java
@@ -0,0 +1,10 @@
+package com.example.sondrehj.familymedicinereminderclient.Models;
+
+/**
+ * Created by nikolai on 24/02/16.
+ */
+public class MedicationStorage {
+    String ownerId;
+    String name;
+    Double count;
+}
