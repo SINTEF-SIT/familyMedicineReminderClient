076d3b44a251b71c93710e7a6526f82e217b5ad9
Nikolai Hegelstad <nikolai@hegelstad.net>
2016-02-26 15:12:45 +0100

    deleted mediciation storage, use mediciation instead

---
+++ app/src/main/java/com/example/sondrehj/familymedicinereminderclient/models/Medication.java
@@ -0,0 +1,34 @@
+package com.example.sondrehj.familymedicinereminderclient.models;
+
+/**
+ * Created by nikolai on 24/02/16.
+ */
+public class Medication {
+    String ownerId;
+    String name;
+    Double count;
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCount() {
+        return count;
+    }
+
+    public void setCount(Double count) {
+        this.count = count;
+    }
+}
