a85e10ba58e24766c351673d7c98a4a97825f796
hegelstad <nikolai@hegelstad.net>
2016-05-19 13:09:54 +0200

    fixed typo

--- app/src/main/java/com/example/sondrehj/familymedicinereminderclient/api/MyCyFAPPServiceAPI.java
+++ app/src/main/java/com/example/sondrehj/familymedicinereminderclient/api/MyCyFAPPServiceAPI.java
@@ -80,7 +80,7 @@ public interface MyCyFAPPServiceAPI {
     @GET("user/{userID}/lastSeen")
     Call<User> getLastSeenStatus(@Path("userID") String userID);
 
-    @DELETE("user/{userID}/removechildren")
+    @DELETE("user/{userID}/children")
     Call<User> removeChildrenFromGuardian(@Path("userID") String userID);
 
 
